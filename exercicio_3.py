2a69e349c0913d37383622a1a605def577fbbf68
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:44:48 -0300

    Add files via upload

---
+++ exercicio_3.py
@@ -0,0 +1,12 @@
+print("CÁLCULO DE POTENCIAÇÃO")
+print()
+
+base=int(input("Digite ao número Base: "))
+print()
+
+expoente=int(input("Digite o número do Expoente: "))
+print()
+
+resultado= base ** expoente
+
+print("O resultado da potenciação é:" ,resultado, ".")
