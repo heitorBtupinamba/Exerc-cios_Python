15dad4d54ac1f67c947424a1f8ac16a07d8e1cad
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:45:15 -0300

    Add files via upload

---
+++ exercicio_5.py
@@ -0,0 +1,13 @@
+print("PROGRAMA DE ADIÇÃO")
+print()
+
+numerointeiro1=int(input("Digite um número inteiro: "))
+print("")
+numerointeiro2=int(input("Digite um segundo número inteiro: "))
+
+resultado= numerointeiro1 + numerointeiro2
+
+print()
+
+# Utilizando f-string para inserir o resultado diretamente na string formatada
+print(f"O resultado da Adição desses números é: {resultado}.")
