178f4478ec0f33293df244d84ee5e8de3266b280
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:45:36 -0300

    Add files via upload

---
+++ exercicio_6.py
@@ -0,0 +1,9 @@
+print(" PROGRAMA DE CONVERSÃO CELSIUS PARA FAHRENHEIT")
+print()
+
+celsius=float(input(" Digite a temperatura em graus °C (Celsius): "))
+print()
+
+fahrenheit=  (9 * celsius + 160) / 5
+
+print(f" Essa temperatura em Fahrenheit é: {fahrenheit:.1f}°F.")
