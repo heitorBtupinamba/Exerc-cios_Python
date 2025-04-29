b644a38fba1edab273fd51125841b84844abd2be
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:44:36 -0300

    Add files via upload

---
+++ exercicio_2.py
@@ -0,0 +1,10 @@
+print("PROGRAMA DE CÁLCULO DE ÁREA DE CIRCUNFERÊNCIA")
+print("")
+
+raio=float(input("Digite o Raio da Circunferência: "))
+
+area = 3.14159 * (raio**2)
+
+print()
+print("A Área da Circunferência é: {:.2f}.".format(area))
+
