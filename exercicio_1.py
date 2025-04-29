2003edb6fbef1acf68f71f2bb53e978e2fc7e8d9
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:44:08 -0300

    Add files via upload

---
+++ exercicio_1.py
@@ -0,0 +1,12 @@
+print("PROGRAMA DE MÉDIA DE NOTAS BIMESTRAIS")
+print("")
+
+nota1b=float(input("Digite a nota do primeiro Bimestre: "))
+nota2b=float(input("Digite a nota do segundo Bimestre: "))
+nota3b=float(input("Digite a nota do terceiro Bimestre: "))
+nota4b=float(input("Digite a nota do quarto Bimestre: "))
+
+mediab = float
+mediab = (nota1b+nota2b+nota3b+nota4b)/4
+
+print("A média dos 4 Bimestres é", mediab, ".")
