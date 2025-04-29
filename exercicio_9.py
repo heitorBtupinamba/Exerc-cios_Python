8e38dfa71ef7117c0212cc75c8914432c11d8a78
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:46:30 -0300

    Add files via upload

---
+++ exercicio_9.py
@@ -0,0 +1,15 @@
+print(" PROGRAMA DE CÁLCULO VALOR DO VOLUME DE UMA CAIXA RETANGULAR")
+print()
+
+comprimento=float(input(" Digite o comprimento da caixa: "))
+print()
+
+altura=float(input(" Digite a altura da caixa: "))
+print()
+
+largura=float(input(" Digite a largura da caixa: "))
+print()
+
+volume= comprimento * largura * altura
+
+print(f" O volume desta caixa retangular é: {volume:.1f}.")
